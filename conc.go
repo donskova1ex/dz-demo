74a889a69ed9c4ed56bf0225cd74fa27efd54936
donskova1ex <67426978+donskova1ex@users.noreply.github.com>
2025-06-11 17:35:30 +0500

    Update conc.go

--- conc.go
+++ conc.go
@@ -16,33 +16,31 @@ func sq() {
 
 	wg := &sync.WaitGroup{}
 
-	  wg.Add(1)
+	wg.Add(1)
 	go func() {
-	        defer wg.Done()
-	        defer close(numbers) 
-	        for i := 0; i < 10; i++ {
-	            numbers <- rand.Intn(101)
-	        }
-	    }()
-	
+		defer wg.Done()
+		defer close(numbers)
+		for i := 0; i < 10; i++ {
+			numbers <- rand.Intn(101)
+		}
+	}()
 
-	    wg.Add(1)
-	    go func() {
-	        defer wg.Done()
-	        defer close(squares) 
-	        for num := range numbers {
-	            squares <- num * num
-	        }
-	    }()
-	
-	    // Горутина для вывода результатов
-	    go func() {
-	        for res := range squares {
-	            fmt.Printf("[%d] ", res)
-	        }
-	    }()
-	
-	    wg.Wait()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(squares)
+		for num := range numbers {
+			squares <- num * num
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+	}()
+
+	for result := range squares {
+		fmt.Printf("[%d] ", result)
+	}
 
 	
 }
